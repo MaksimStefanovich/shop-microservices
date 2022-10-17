62e1ee36deb216c75b3c0645d96f556f66697e66
Maksim Stefanovich <maxim.stefanovich85@gmail.com>
2022-10-17 16:02:31 +0200

    separate item amd itemSaveDto

--- product-back/src/main/java/com/stefanovich/productback/service/ItemService.java
+++ product-back/src/main/java/com/stefanovich/productback/service/ItemService.java
@@ -3,29 +3,19 @@ package com.stefanovich.productback.service;
 import com.stefanovich.productback.model.Item;
 import com.stefanovich.productback.model.dto.ItemSaveDto;
 import com.stefanovich.productback.repository.ItemRepository;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
-  private final ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
 
-  public void saveItem(ItemSaveDto itemSaveDto) {
-    itemRepository.insert(Item.builder()
-        .name(itemSaveDto.getName())
-        .price(itemSaveDto.getPrice()).build());
-  }
- @Value("${spring.data.mongodb.username}")
- public String test;
+    public void saveItem(ItemSaveDto itemSaveDto) {
+        itemRepository.insert(Item.builder()
+                .name(itemSaveDto.getName())
+                .price(itemSaveDto.getPrice()).build());
+    }
 
-
-  @PostConstruct
-  public void init(){
-    System.out.println("test = " + test);
-
-  }
 }
